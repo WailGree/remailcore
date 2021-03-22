406a8bc7a68c7bafa2b74c852645afeceecccab7
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-03-22 16:10:59 +0100

    Create DataContext for SqlServer

---
+++ DataAccess/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using RemailCore.Models;
+
+namespace RemailCore.DataAccess
+{
+    class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+        public DbSet<List<Account>> Accounts { get; set; }
+        public DbSet<List<Email>> Emails { get; set; }
+    }
+}
