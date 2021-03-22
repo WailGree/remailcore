9cd8c1daa85cb94fcd7726c468f41c91a3ddee9f
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-03-22 17:29:41 +0100

    Change UId type from UniqueId to int

--- Models/Email.cs
+++ Models/Email.cs
@@ -1,21 +1,25 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using MailKit;
 
 namespace RemailCore.Models
 {
     public class Email
     {
-        public int Id { get; set; }
+        [Key] public int Id { get; set; }
         [Required] public bool Seen { get; set; }
         [Required] [MaxLength(100)] public string Sender { get; set; }
         [Required] [MaxLength(100)] public string Subject { get; set; }
         [Required] public DateTime Date { get; set; }
         [Required] [MaxLength] public string Body { get; set; }
-        [Required] public UniqueId UId { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(24)")]
+        public int UId { get; set; }
 
 
-        public Email(bool seen, string sender, string subject, DateTime date, string body, UniqueId uId)
+        public Email(bool seen, string sender, string subject, DateTime date, string body, int uId)
         {
             Seen = seen;
             Sender = sender.Replace(@"""", String.Empty);
