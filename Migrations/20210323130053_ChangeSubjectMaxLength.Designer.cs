73dce2476bfb9219277edf68af096fcf2808ee04
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-03-23 14:07:57 +0100

    Add ChangeSubjectMaxLength script

--- Migrations/20210323130053_ChangeSubjectMaxLength.Designer.cs
+++ Migrations/20210323130053_ChangeSubjectMaxLength.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RemailCore.DataAccess;
 
 namespace RemailCore.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210323130053_ChangeSubjectMaxLength")]
+    partial class ChangeSubjectMaxLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
